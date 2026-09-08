570c88b174153870c2afb401638559a7bebc0d80
yuriylevenets <73877779+yuriylevenets@users.noreply.github.com>
2026-09-08 18:02:27 +0000

    feat: implement General Info step for Become a Tutor/Student popup (#74)
    - add First Name / Last Name fields with validation and pre-fill from user data
    - add Country / City text fields (dropdown with LocationService will be added in #80)
    - add professional status textarea with 100 char limit and counter
    - update generalInfo.title translation to match mockup
    - match styles/spacing with Figma design

--- src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
+++ src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
@@ -1,12 +1,108 @@
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
+
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+import AppTextField from '~/components/app-text-field/AppTextField'
+import AppTextArea from '~/components/app-text-area/AppTextArea'
+
+import useForm from '~/hooks/use-form'
+import useAxios from '~/hooks/use-axios'
+import { useStepContext } from '~/context/step-context'
+import { userService } from '~/services/user-service'
+import {
+  initialValues,
+  validations
+} from '~/components/user-steps-wrapper/constants'
+
+import generalInfoImg from '~/assets/img/tutor-home-page/become-tutor/general-info.svg'
 
 import { styles } from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles'
 
-const GeneralInfoStep = ({ btnsBox }) => {
+const fieldWidth = { width: '208px' }
+
+const GeneralInfoStep = ({
+  btnsBox,
+  stepLabel,
+  isUserFetched,
+  setIsUserFetched
+}) => {
+  const { t } = useTranslation()
+  const { userId, userRole } = useSelector((state) => state.appMain)
+  const { handleStepData } = useStepContext()
+
+  const { data, errors, handleInputChange, handleBlur, handleDataChange } =
+    useForm({ initialValues, validations })
+
+  useAxios({
+    service: () => userService.getUserById(userId, userRole),
+    fetchOnMount: !isUserFetched,
+    onResponse: (responseData) => {
+      handleDataChange({
+        firstName: responseData.firstName,
+        lastName: responseData.lastName
+      })
+      setIsUserFetched(true)
+    }
+  })
+
+  useEffect(() => {
+    handleStepData(stepLabel, data, errors)
+  }, [data, errors])
+
   return (
     <Box sx={styles.container}>
-      GeneralInfo step
-      {btnsBox}
+      <Box alt='general info' component='img' src={generalInfoImg} />
+
+      <Box sx={styles.formContainer}>
+        <Typography sx={styles.title}>
+          {t('becomeTutor.generalInfo.title')}
+        </Typography>
+
+        <Box sx={styles.namesRow}>
+          <AppTextField
+            autoFocus
+            errorMsg={t(errors.firstName)}
+            label={`${t('common.labels.firstName')} *`}
+            onBlur={handleBlur('firstName')}
+            onChange={handleInputChange('firstName')}
+            sx={fieldWidth}
+            value={data.firstName}
+          />
+          <AppTextField
+            errorMsg={t(errors.lastName)}
+            label={`${t('common.labels.lastName')} *`}
+            onBlur={handleBlur('lastName')}
+            onChange={handleInputChange('lastName')}
+            sx={fieldWidth}
+            value={data.lastName}
+          />
+        </Box>
+
+        <Box sx={styles.locationRow}>
+          <AppTextField label={t('common.labels.country')} sx={fieldWidth} />
+          <AppTextField label={t('common.labels.city')} sx={fieldWidth} />
+        </Box>
+
+        <Box sx={styles.textAreaWrapper}>
+          <AppTextArea
+            fullWidth
+            maxLength={100}
+            onChange={handleInputChange('professionalSummary')}
+            placeholder={t('becomeTutor.generalInfo.textFieldLabel')}
+            sx={styles.textarea}
+            value={data.professionalSummary}
+          />
+
+          <Typography sx={styles.helperText}>
+            {t('becomeTutor.generalInfo.helperText')}
+          </Typography>
+        </Box>
+
+        {btnsBox}
+      </Box>
     </Box>
   )
 }
--- src/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles.js
+++ src/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles.js
@@ -3,9 +3,48 @@ import { fadeAnimation } from '~/styles/app-theme/custom-animations'
 export const styles = {
   container: {
     display: 'flex',
-    justifyContent: 'space-around',
+    gap: '139px',
+    alignItems: 'flex-start',
     height: { sm: '485px' },
     paddingBottom: { xs: '30px', sm: '0' },
     ...fadeAnimation
+  },
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '30px',
+    width: '432px'
+  },
+  title: {
+    typography: 'body1',
+    color: '#263238'
+  },
+  namesRow: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  locationRow: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  textAreaWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
+    width: '100%'
+  },
+  textarea: {
+    width: '100%',
+    '& .MuiInputBase-root': {
+      height: '117px',
+      borderRadius: '4px',
+      '& fieldset': {
+        borderColor: '#90A4AE'
+      }
+    }
+  },
+  helperText: {
+    color: 'text.secondary',
+    typography: 'body2'
   }
 }
