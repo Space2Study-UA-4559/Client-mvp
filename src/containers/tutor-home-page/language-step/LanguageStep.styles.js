1008e94bd5141ca304735533d0617030fc959ba7
kostya <paholokkostantin@gmail.com>
2026-09-14 14:48:56 +0300

    implement language selection

--- src/containers/tutor-home-page/language-step/LanguageStep.styles.js
+++ src/containers/tutor-home-page/language-step/LanguageStep.styles.js
@@ -2,10 +2,48 @@ import { fadeAnimation } from '~/styles/app-theme/custom-animations'
 
 export const styles = {
   container: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    gap: '40px',
-    height: { sm: '485px' },
+    display: 'grid',
+    gridTemplateColumns: {
+      xs: 'minmax(0, 1fr)',
+      sm: 'repeat(2, minmax(0, 1fr))'
+    },
+    gridTemplateAreas: {
+      xs: '"description" "image" "select" "buttons"',
+      sm: '"image description" "image select" "image buttons"'
+    },
+    gridTemplateRows: { xs: 'auto auto auto 1fr', sm: 'auto 1fr auto' },
+    columnGap: '40px',
+    rowGap: '24px',
+    width: '100%',
+    minHeight: { sm: '485px' },
     ...fadeAnimation
+  },
+  description: {
+    gridArea: 'description'
+  },
+  image: {
+    gridArea: 'image',
+    width: '100%',
+    maxWidth: { xs: '240px', sm: '400px' },
+    alignSelf: 'center',
+    justifySelf: 'center'
+  },
+  select: {
+    gridArea: 'select',
+    alignSelf: 'start'
+  },
+  buttons: {
+    gridArea: 'buttons',
+    alignSelf: 'end'
+  },
+  listbox: {
+    boxSizing: 'border-box',
+    maxHeight: '288px',
+    overflowY: 'auto',
+    padding: 0,
+    '& .MuiAutocomplete-option': {
+      minHeight: '48px',
+      height: '48px'
+    }
   }
 }
