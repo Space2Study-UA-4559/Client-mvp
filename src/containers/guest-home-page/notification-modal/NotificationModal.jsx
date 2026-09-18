580751a50b91d34430faee7757c92b07edb46fa7
pasichnykann <pasichnuk.a12@gmail.com>
2026-09-18 22:06:46 +0300

    Create informational pop-up window

--- src/containers/guest-home-page/notification-modal/NotificationModal.jsx
+++ src/containers/guest-home-page/notification-modal/NotificationModal.jsx
@@ -1,25 +1,38 @@
-import { Box } from '@mui/material'
-
+import CloseIcon from '@mui/icons-material/Close'
+import { Box, IconButton } from '@mui/material'
+import { useTranslation } from 'react-i18next'
 import AppButton from '~/components/app-button/AppButton'
 import ImgTitleDescription from '~/components/img-title-description/ImgTitleDescription'
 import { styles } from '~/containers/guest-home-page/notification-modal/NotificationModal.styles'
+import info from '~/assets/img/guest-home-page/info.svg'
 
-const NotificationModal = ({
-  description,
-  buttonTitle,
-  title,
-  img,
-  onClose
-}) => {
+const NotificationModal = ({ onClose, email }) => {
+  const { t } = useTranslation()
+  const handleClose = (event) => {
+    event.stopPropagation()
+    onClose()
+  }
+  const handleContentClick = (event) => {
+    event.stopPropagation()
+  }
   return (
-    <Box sx={styles.root}>
+    <Box onClick={handleContentClick} sx={styles.root}>
+      <IconButton
+        aria-label='close'
+        onClick={handleClose}
+        sx={styles.iconButton}
+      >
+        <CloseIcon />
+      </IconButton>
       <ImgTitleDescription
-        description={description}
-        img={img}
+        description={`${t('signup.confirmEmailMessage')} ${email} ${t(
+          'signup.confirmEmailDesc'
+        )}`}
+        img={info}
         style={styles.imgTitleDesc}
-        title={title}
+        title={t('signup.confirmEmailTitle')}
       />
-      <AppButton onClick={onClose}>{buttonTitle}</AppButton>
+      <AppButton onClick={onClose}>{t('common.confirmButton')}</AppButton>
     </Box>
   )
 }
